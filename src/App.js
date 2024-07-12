52ee7c7f472702d2ffd22753fe30918169c33116
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-07-12 08:24:12 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import VideoSection from './Videos'; // Keeping VideoSection from HEAD branch
 import Landingpage from './Landingpage';
+import Blog from './Blog';
 
 function App() {
   return (
     <div className="App">
       <Landingpage/>
       <VideoSection /> 
+      <Blog/>
       {/* <Footer/> */}
     </div>
   );
